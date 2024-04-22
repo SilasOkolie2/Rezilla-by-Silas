ec7c2813c50572b5ff5c510ef15c73ef3b1b6faf
Silas Okolie <159030284+SilasOkolie2@users.noreply.github.com>
2024-04-22 16:39:04 +0100

    I made the Body which is the Hero page responsive to smaller devices.

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -12,33 +12,35 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+  
     autoplaySpeed: 3000,
   };
 
   return (
     <Slider {...settings}>
-      <div className="w-[350px] h-[250px]">
+      
+      <div className="sm:w-[350px] h-[250px]  w-[100px] ">
         <img
           src="public\Rectangle 27.png"
           alt="Image 1"
           className="w-full h-full"
         />
       </div>
-      <div className="w-[350px] h-[250px]">
+      <div className="sm:w-[350px] h-[250px] w-[100px]">
         <img
           src="public\Rectangle 26.png"
           alt="Image 2"
           className="w-full h-full"
         />
       </div>
-      <div className="w-[350px] h-[250px]">
+      <div className="sm:w-[350px] h-[250px] w-[100px]">
         <img
           src="public\Rectangle 28.png"
           alt="Image 3"
           className="w-full h-full"
         />
       </div>
-      <div className="w-[350px] h-[250px]">
+      <div className="sm:w-[350px] h-[250px] w-[100px]">
         <img
           src="luxury-classic-modern-bedroom-suite-hotel 1.png"
           alt="Image 4"
@@ -51,27 +53,29 @@ const Carousel = () => {
 const Body = () => {
   return (
     <>
-      <div className="hero min-h-screen w-[100%] relative h-[750px] pb-10 bg-gradient-to-t from-[rgb(170,180,220)] to-[#4361EE bg-opacity-30] gap-6">
-        <div className="hero-content pb-14 flex-wrap gap-20 ">
-          <div className="flex-col items-left w-[550px] h-[300px]">
-            <div className="text-center lg:text-left p-">
+      <div className="hero min-h-screen w-[100%] pb-16 sm:h-[750px] pt-20 sm:pt-0 sm:pb-10 bg-gradient-to-t from-[rgb(170,180,220)] to-[#4361EE bg-opacity-30] gap-6">
+        <div className=" sm:pt-10 flex sm:flex-row flex-col sm:gap-10 gap-40 p-5">
+          <div className="flex flex-col items-left justify-start items- w-[550px] h-[300px]">
+            <div className="text-left lg:text-left ">
               <h1 className="text-lg sm:text-xl font-semibold text-[19.3px] text-[#4361EE]">
                 REAL ESTATE
               </h1>
-              <h1 className="text-[50px] w-full lg:w-[500px] font-bold leading-[60px] text-[#2B2B2B]">
+              <h1 className="sm:text-[50px] text-[22px] w-full lg:w-[500px] font-bold leading-[60px] text-[#2B2B2B]">
                 Find a perfect home you love..!
               </h1>
-              <p className="py-1 text-base text-[1.1rem] leading-[20px] sm:leading-[24px] w-full sm:w-[460px] text-[#2B2B2B]">
+              <p className="py-1 text-base text-[1.1rem] leading-[25px]  sm:leading-[24px] w-[360px] sm:w-[460px] text-[#2B2B2B]">
                 Etiam eget elementum elit. Aenean dignissim dapibus vestibulum.
                 Integer a dolor eu sapien sodales vulputate ac in purus.
               </p>
             </div>
+            <div className="sm:w-[450px] h-[250px] w-[350px]">
             <Carousel />
+            </div>
           </div>
 
           {/* FORM */}
-          <div className="card w-[580px] h-[285px] max-w-sm shadow-2xl">
-            <div className=" bg-white rounded-[30px] ">
+          <div className=" flex flex-col">
+            <div className=" card w-[350px] h-[450px] shadow-2xl bg-white rounded-[30px]">
               <form className="card-body flex flex-col space-y-4">
                 <div className="flex justify-center border-b border-gray-500 gap-9">
                   <label className="label">
