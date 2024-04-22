03c3437bc4f40e7d1835e7137386065c3f104f60
Silas Okolie <159030284+SilasOkolie2@users.noreply.github.com>
2024-04-22 18:47:44 +0100

    At the Subbody, i Changed the aos style and made the component responsive to small devices.

--- src/Components/Subbody.jsx
+++ src/Components/Subbody.jsx
@@ -17,23 +17,23 @@ const Subbody = () => {
           <div className="text-16px leading-24px">
             <span> Trusted by 100+ Companies across the globe! </span>
           </div>
-          <div className="flex items-center justify-center gap-12 p-6 filter grayscale">
-            <div className="carousel-item" data-aos="zoom-in-left">
+          <div className="flex flex-wrap items-center justify-center gap-12 p-6 filter grayscale">
+            <div className="carousel-item" data-aos="slide-right">
               <img src="public/image 1.png" alt="google" />
             </div>
-            <div className="carousel-item" data-aos="zoom-in-left">
+            <div className="carousel-item" data-aos="slide-right">
               <img src="public/image 2.png" alt="amazon" />
             </div>
-            <div className="carousel-item" data-aos="zoom-in-left">
+            <div className="carousel-item" data-aos="slide-right">
               <img src="public/image 3.png" alt="logitech" />
             </div>
-            <div className="carousel-item" data-aos="zoom-in-left">
+            <div className="carousel-item" data-aos="slide-right">
               <img src="public/image 4.png" alt="spotify" />
             </div>
-            <div className="carousel-item" data-aos="zoom-in-left">
+            <div className="carousel-item" data-aos="slide-right">
               <img src="public/image 6.png" alt="netflix" />
             </div>
-            <div className="carousel-item" data-aos="zoom-in-left">
+            <div className="carousel-item" data-aos="slide-right">
               <img src="public/image 5.png" alt="samsung" />
             </div>
           </div>
